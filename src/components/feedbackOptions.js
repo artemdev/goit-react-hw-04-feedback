0068eeaa7d6235bf7eb3f0f13455e35d9f5997fd
Artem Zymovets <artwayprojects@gmail.com>
2021-01-10 13:43:05 +0200

    changes

--- src/components/feedbackOptions.js
+++ src/components/feedbackOptions.js
@@ -1,12 +1,15 @@
-
-export default function FeedbackOptions ({onLeaveFeedback}) {
-
-        return(
-            <div>
-                <button name="neutral" onClick={onLeaveFeedback}>Neutral</button>
-                <button name="bad" onClick={onLeaveFeedback}>Bad</button>
-                <button name="good" onClick={onLeaveFeedback}>Good!</button> 
-            </div>
-        )
-
+export default function FeedbackOptions({ onLeaveFeedback }) {
+  return (
+    <div>
+      <button name="neutral" onClick={onLeaveFeedback}>
+        Neutral
+      </button>
+      <button name="bad" onClick={onLeaveFeedback}>
+        Bad
+      </button>
+      <button name="good" onClick={onLeaveFeedback}>
+        Good!
+      </button>
+    </div>
+  );
 }
